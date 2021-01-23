1c5e8bdda7d573f3ae9c51955636ba0fb842b19d
Igor Lenko <mk2008@mail.ru>
2021-01-23 20:01:13 +0300

    init:the task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(str.length%2 === 0){
+    let arr = [];
+    
+    [...str].forEach((e,i) => {
+      bracketsConfig.forEach(elem => {
+        if(elem[0] !== elem[1]){
+          if(e === elem[0]){
+            arr.push(e);
+          } else if(e === elem[1]){
+            if(arr[arr.length - 1] === elem[0]){
+              arr.pop();
+            } else {
+              arr.push(e);
+            }
+          }
+        } else {
+          if(e === elem[0]){
+            (arr[arr.length-1] === e && arr.length !== 0)? arr.pop() : arr.push(e);
+          }
+        }
+
+      })
+    })
+    
+    return (arr.length === 0);
+  }else{
+    return false;
+  }
 }
